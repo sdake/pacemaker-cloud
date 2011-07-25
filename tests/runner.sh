114683186de77cd843aa2dddffb20e98de5ed154
Angus Salkeld <asalkeld@redhat.com>
2011-07-25 21:30:25 +1000

    TEST: add recovery tests
    Signed-off-by: Angus Salkeld <asalkeld@redhat.com>

--- tests/runner.sh
+++ tests/runner.sh
@@ -3,11 +3,14 @@
 set +e
 
 systemctl --system daemon-reload
+systemctl start pcmkc-qpidd.service
 systemctl start pcmkc-cped.service
+
+sleep 5
 
 #./basic.sh rhel61 x86_64
 ./basic.sh F15 x86_64
-#./basic.sh F14 x86_64
+./basic.sh F14 x86_64
 
 systemctl stop pcmkc-cped.service
 systemctl stop pcmkc-vmlauncher.service
