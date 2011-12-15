de38d1c466e7bd66b92b805b40c89ea4274bab55
Angus Salkeld <asalkeld@redhat.com>
2011-12-16 10:26:33 +1100

    shell: run a script if deployable state changes
    Signed-off-by: Angus Salkeld <asalkeld@redhat.com>

--- src/pcloudsh/eventreceiver.py
+++ src/pcloudsh/eventreceiver.py
@@ -16,14 +16,24 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 #
+import os
 import cqpid
 import qmf2
 import threading
+from pcloudsh import pcmkconfig
 
 class EventReceiver(qmf2.ConsoleHandler):
 
     def __init__(self, session):
         qmf2.ConsoleHandler.__init__(self, session)
+        self.conf = pcmkconfig.Config()
 
     def eventRaised(self, agent, data, timestamp, severity):
-        print "Event: %r" % (data.getProperties())
+        props = data.getProperties()
+        print "Event: %r" % (props)
+        if not 'assembly' in props:
+            dep = props['deployable']
+            state = props['state']
+            script = '%s/%s.sh' % (self.conf.dbdir, dep)
+            if os.access(script, os.R_OK):
+                os.system('%s %s' % (script, state))
