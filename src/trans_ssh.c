4f10635c9d50e8ce4bf349a61bdae5b8e5956c60
Steven Dake <sdake@redhat.com>
2012-02-26 07:22:45 -0700

    Rename ta_ssh to trans_ssh
    Signed-off-by: Steven Dake <sdake@redhat.com>

--- src/trans_ssh.c
+++ src/trans_ssh.c
@@ -76,7 +76,7 @@ struct ssh_operation {
 	char command[RESOURCE_NAME_MAX + 26];
 };
 
-struct ta_ssh {
+struct trans_ssh {
 	int fd;
 	enum ssh_state ssh_state;
 	qb_loop_timer_handle keepalive_timer;
@@ -104,20 +104,20 @@ static void ssh_op_complete(struct ssh_operation *ssh_op)
 static void assembly_ssh_exec(void *data)
 {
 	struct ssh_operation *ssh_op = (struct ssh_operation *)data;
-	struct ta_ssh *ta_ssh = (struct ta_ssh *)ssh_op->assembly->transport_assembly;
+	struct trans_ssh *trans_ssh = (struct trans_ssh *)ssh_op->assembly->transport_assembly;
 	int rc;
 	char buffer[4096];
 	ssize_t rc_read;
 
 	qb_enter();
 
-	assert (ta_ssh->ssh_state == SSH_SESSION_CONNECTED);
+	assert (trans_ssh->ssh_state == SSH_SESSION_CONNECTED);
 
 	switch (ssh_op->ssh_exec_state) {
 	case SSH_CHANNEL_OPEN:
-		ssh_op->channel = libssh2_channel_open_session(ta_ssh->session);
+		ssh_op->channel = libssh2_channel_open_session(trans_ssh->session);
 		if (ssh_op->channel == NULL) {
-			rc = libssh2_session_last_errno(ta_ssh->session);
+			rc = libssh2_session_last_errno(trans_ssh->session);
 			if (rc == LIBSSH2_ERROR_EAGAIN) {
 				goto job_repeat_schedule;
 			}
@@ -284,7 +284,7 @@ job_repeat_schedule:
 static void ssh_timeout(void *data)
 {
 	struct ssh_operation *ssh_op = (struct ssh_operation *)data;
-	struct ta_ssh *ta_ssh = (struct ta_ssh *)ssh_op->assembly->transport_assembly;
+	struct trans_ssh *trans_ssh = (struct trans_ssh *)ssh_op->assembly->transport_assembly;
 	struct qb_list_head *list_temp;
 	struct qb_list_head *list;
 	struct ssh_operation *ssh_op_del;
@@ -292,7 +292,7 @@ static void ssh_timeout(void *data)
 	qb_enter();
 
 	qb_log(LOG_NOTICE, "ssh service timeout on assembly '%s'", ssh_op->assembly->name);
-	qb_list_for_each_safe(list, list_temp, &ta_ssh->ssh_op_head) {
+	qb_list_for_each_safe(list, list_temp, &trans_ssh->ssh_op_head) {
 		ssh_op_del = qb_list_entry(list, struct ssh_operation, list);
 		qb_loop_timer_del(NULL, ssh_op_del->ssh_timer);
 		qb_loop_job_del(NULL, QB_LOOP_LOW, ssh_op_del,
@@ -308,14 +308,14 @@ static void ssh_timeout(void *data)
 
 static void ssh_keepalive_send(void *data)
 {
-	struct ta_ssh *ta_ssh = (struct ta_ssh *)data;
+	struct trans_ssh *trans_ssh = (struct trans_ssh *)data;
 	int seconds_to_next;
 
 	qb_enter();
 
-	libssh2_keepalive_send(ta_ssh->session, &seconds_to_next);
+	libssh2_keepalive_send(trans_ssh->session, &seconds_to_next);
 	qb_loop_timer_add(NULL, QB_LOOP_LOW, seconds_to_next * 1000 * QB_TIME_NS_IN_MSEC,
-		ta_ssh, ssh_keepalive_send, &ta_ssh->keepalive_timer);
+		trans_ssh, ssh_keepalive_send, &trans_ssh->keepalive_timer);
 
 	qb_leave();
 }
@@ -323,29 +323,29 @@ static void ssh_keepalive_send(void *data)
 static void ssh_assembly_connect(void *data)
 {
 	struct assembly *assembly = (struct assembly *)data;
-	struct ta_ssh *ta_ssh = (struct ta_ssh *)assembly->transport_assembly;
+	struct trans_ssh *trans_ssh = (struct trans_ssh *)assembly->transport_assembly;
 	char name[PATH_MAX];
 	char name_pub[PATH_MAX];
 	int rc;
 
 	qb_enter();
 
-	switch (ta_ssh->ssh_state) {
+	switch (trans_ssh->ssh_state) {
 	case SSH_SESSION_INIT:
-		ta_ssh->session = libssh2_session_init();
-		if (ta_ssh->session == NULL) {
+		trans_ssh->session = libssh2_session_init();
+		if (trans_ssh->session == NULL) {
 			goto job_repeat_schedule;
 		}
 
-		libssh2_session_set_blocking(ta_ssh->session, 0);
-		ta_ssh->ssh_state = SSH_SESSION_STARTUP;
+		libssh2_session_set_blocking(trans_ssh->session, 0);
+		trans_ssh->ssh_state = SSH_SESSION_STARTUP;
 
 		/*
                  * no break here is intentional
 		 */
 
 	case SSH_SESSION_STARTUP:
-		rc = libssh2_session_startup(ta_ssh->session, ta_ssh->fd);
+		rc = libssh2_session_startup(trans_ssh->session, trans_ssh->fd);
 		if (rc == LIBSSH2_ERROR_EAGAIN) {
 			goto job_repeat_schedule;
 		}
@@ -358,23 +358,23 @@ static void ssh_assembly_connect(void *data)
 		/*
                  * no break here is intentional
 		 */
-		ta_ssh->ssh_state = SSH_KEEPALIVE_CONFIG;
+		trans_ssh->ssh_state = SSH_KEEPALIVE_CONFIG;
 
 	case SSH_KEEPALIVE_CONFIG:
-		libssh2_keepalive_config(ta_ssh->session, 1, KEEPALIVE_TIMEOUT);
-		qb_loop_job_add(NULL, QB_LOOP_LOW, ta_ssh, ssh_keepalive_send);
+		libssh2_keepalive_config(trans_ssh->session, 1, KEEPALIVE_TIMEOUT);
+		qb_loop_job_add(NULL, QB_LOOP_LOW, trans_ssh, ssh_keepalive_send);
 
 		/*
                  * no break here is intentional
 		 */
-		ta_ssh->ssh_state = SSH_USERAUTH_PUBLICKEY_FROMFILE;
+		trans_ssh->ssh_state = SSH_USERAUTH_PUBLICKEY_FROMFILE;
 
 	case SSH_USERAUTH_PUBLICKEY_FROMFILE:
 		snprintf (name, PATH_MAX, "/var/lib/pacemaker-cloud/keys/%s",
 			assembly->name);
 		snprintf (name_pub, PATH_MAX, "/var/lib/pacemaker-cloud/keys/%s.pub",
 			assembly->name);
-		rc = libssh2_userauth_publickey_fromfile(ta_ssh->session,
+		rc = libssh2_userauth_publickey_fromfile(trans_ssh->session,
 			"root", name_pub, name, "");
 		if (rc == LIBSSH2_ERROR_EAGAIN) {
 			goto job_repeat_schedule;
@@ -391,7 +391,7 @@ static void ssh_assembly_connect(void *data)
 		assembly_healthcheck(assembly);
 
 		recover_state_set(&assembly->recover, RECOVER_STATE_RUNNING);
-		ta_ssh->ssh_state = SSH_SESSION_CONNECTED;
+		trans_ssh->ssh_state = SSH_SESSION_CONNECTED;
 
 	case SSH_SESSION_CONNECTED:
 		break;
@@ -423,13 +423,13 @@ ssh_nonblocking_exec(struct assembly *assembly,
 {
 	va_list ap;
 	struct ssh_operation *ssh_op;
-	struct ta_ssh *ta_ssh = assembly->transport_assembly;
+	struct trans_ssh *trans_ssh = assembly->transport_assembly;
 
 	qb_enter();
 	/*
 	 * Only execute an opperation when in the connected state
 	 */
-	if (ta_ssh->ssh_state != SSH_SESSION_CONNECTED) {
+	if (trans_ssh->ssh_state != SSH_SESSION_CONNECTED) {
 		qb_leave();
 		return;
 	}
@@ -453,7 +453,7 @@ ssh_nonblocking_exec(struct assembly *assembly,
 	ssh_op->resource = resource;
 	ssh_op->completion_func = completion_func;
 	qb_list_init(&ssh_op->list);
-	qb_list_add_tail(&ssh_op->list, &ta_ssh->ssh_op_head);
+	qb_list_add_tail(&ssh_op->list, &trans_ssh->ssh_op_head);
 
 	if (op) {
 		pe_resource_ref(ssh_op->op);
@@ -513,20 +513,20 @@ static void assembly_healthcheck(void *data)
 static void connect_execute(void *data)
 {
 	struct assembly *assembly = (struct assembly *)data;
-	struct ta_ssh *ta_ssh = (struct ta_ssh *)assembly->transport_assembly;
+	struct trans_ssh *trans_ssh = (struct trans_ssh *)assembly->transport_assembly;
 	int rc;
 	int flags;
 
 	qb_enter();
 
-	rc = connect(ta_ssh->fd, (struct sockaddr*)(&ta_ssh->sin),
+	rc = connect(trans_ssh->fd, (struct sockaddr*)(&trans_ssh->sin),
 		sizeof (struct sockaddr_in));
 	if (rc == 0) {
-		flags = fcntl(ta_ssh->fd, F_GETFL, 0);
-		fcntl(ta_ssh->fd, F_SETFL, flags | (~O_NONBLOCK));
+		flags = fcntl(trans_ssh->fd, F_GETFL, 0);
+		fcntl(trans_ssh->fd, F_SETFL, flags | (~O_NONBLOCK));
 		qb_log(LOG_NOTICE, "Connected to assembly '%s'",
 			assembly->name);
-		ta_ssh->ssh_state = SSH_SESSION_INIT;
+		trans_ssh->ssh_state = SSH_SESSION_INIT;
 		qb_loop_job_add(NULL, QB_LOOP_LOW, assembly, ssh_assembly_connect);
 	} else {
 		qb_loop_job_add(NULL, QB_LOOP_LOW, assembly, connect_execute);
@@ -537,11 +537,11 @@ static void connect_execute(void *data)
 
 void ta_del(void *ta)
 {
-	struct ta_ssh *ta_ssh = (struct ta_ssh *)ta;
+	struct trans_ssh *trans_ssh = (struct trans_ssh *)ta;
 
 	qb_enter();
 
-	qb_loop_timer_del(NULL, ta_ssh->keepalive_timer);
+	qb_loop_timer_del(NULL, trans_ssh->keepalive_timer);
 
 	qb_leave();
 }
@@ -587,7 +587,7 @@ void*
 ta_connect(struct assembly * a)
 {
 	unsigned long hostaddr;
-        struct ta_ssh *ta_ssh;
+        struct trans_ssh *trans_ssh;
 	int flags;
 
 	qb_enter();
@@ -597,18 +597,18 @@ ta_connect(struct assembly * a)
 	}
 	assert(ssh_init_rc == 0);
 
-	ta_ssh = calloc(1, sizeof(struct ta_ssh));
-	a->transport_assembly = ta_ssh;
+	trans_ssh = calloc(1, sizeof(struct trans_ssh));
+	a->transport_assembly = trans_ssh;
 
 	hostaddr = inet_addr(a->address);
-	ta_ssh->fd = socket(AF_INET, SOCK_STREAM, 0);
-	flags = fcntl(ta_ssh->fd, F_GETFL, 0);
-	fcntl(ta_ssh->fd, F_SETFL, flags | O_NONBLOCK);
-	ta_ssh->sin.sin_family = AF_INET;
-	ta_ssh->sin.sin_port = htons(22);
-	ta_ssh->sin.sin_addr.s_addr = hostaddr;
-	ta_ssh->ssh_state = SSH_SESSION_CONNECTING;
-	qb_list_init(&ta_ssh->ssh_op_head);
+	trans_ssh->fd = socket(AF_INET, SOCK_STREAM, 0);
+	flags = fcntl(trans_ssh->fd, F_GETFL, 0);
+	fcntl(trans_ssh->fd, F_SETFL, flags | O_NONBLOCK);
+	trans_ssh->sin.sin_family = AF_INET;
+	trans_ssh->sin.sin_port = htons(22);
+	trans_ssh->sin.sin_addr.s_addr = hostaddr;
+	trans_ssh->ssh_state = SSH_SESSION_CONNECTING;
+	qb_list_init(&trans_ssh->ssh_op_head);
 
 	qb_log(LOG_NOTICE, "Connection in progress to assembly '%s'",
 		a->name);
@@ -616,12 +616,12 @@ ta_connect(struct assembly * a)
 	qb_loop_job_add(NULL, QB_LOOP_LOW, a, connect_execute);
 
 	qb_leave();
-	return ta_ssh;
+	return trans_ssh;
 }
 
 void ta_disconnect(struct assembly *a)
 {
-	struct ta_ssh *ta_ssh = (struct ta_ssh *)a->transport_assembly;
+	struct trans_ssh *trans_ssh = (struct trans_ssh *)a->transport_assembly;
 	struct qb_list_head *list_temp;
 	struct qb_list_head *list;
 	struct ssh_operation *ssh_op_del;
@@ -631,21 +631,21 @@ void ta_disconnect(struct assembly *a)
 	/*
 	 * Delete a transport connection in progress
 	 */
-	if (ta_ssh->ssh_state == SSH_SESSION_INIT) {
+	if (trans_ssh->ssh_state == SSH_SESSION_INIT) {
 		qb_loop_job_del(NULL, QB_LOOP_LOW, a, connect_execute);
 	}
 
 	/*
 	 * Delete a transport attempting an SSH connection
 	 */
-	if (ta_ssh->ssh_state != SSH_SESSION_CONNECTED) {
+	if (trans_ssh->ssh_state != SSH_SESSION_CONNECTED) {
 		qb_loop_job_del(NULL, QB_LOOP_LOW, a, ssh_assembly_connect);
 	}
 
 	/*
 	 * Delete any outstanding ssh operations
 	 */
-	qb_list_for_each_safe(list, list_temp, &ta_ssh->ssh_op_head) {
+	qb_list_for_each_safe(list, list_temp, &trans_ssh->ssh_op_head) {
 		ssh_op_del = qb_list_entry(list, struct ssh_operation, list);
 		qb_loop_timer_del(NULL, ssh_op_del->ssh_timer);
 		qb_loop_job_del(NULL, QB_LOOP_LOW, ssh_op_del,
@@ -659,13 +659,13 @@ void ta_disconnect(struct assembly *a)
 	 * Free the SSH session associated with this transport
 	 * there are no breaks intentionally in this switch
 	 */
-	switch (ta_ssh->ssh_state) {
+	switch (trans_ssh->ssh_state) {
 	case SSH_SESSION_CONNECTED:
 	case SSH_SESSION_STARTUP:
-		libssh2_session_free(ta_ssh->session);
+		libssh2_session_free(trans_ssh->session);
 	case SSH_USERAUTH_PUBLICKEY_FROMFILE:
 	case SSH_KEEPALIVE_CONFIG:
-		qb_loop_timer_del(NULL, ta_ssh->keepalive_timer);
+		qb_loop_timer_del(NULL, trans_ssh->keepalive_timer);
 	case SSH_SESSION_INIT:
 		qb_loop_job_del(NULL, QB_LOOP_LOW, a, ssh_assembly_connect);
 	default:
@@ -673,6 +673,6 @@ void ta_disconnect(struct assembly *a)
 	}
 
 	free(a->transport_assembly);
-	close(ta_ssh->fd);
+	close(trans_ssh->fd);
 	qb_leave();
 }
