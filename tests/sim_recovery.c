a86e21a18d4bf145e80a459f545edf80ab1a1dde
Steven Dake <sdake@redhat.com>
2012-02-29 09:13:26 -0700

    Add a recovery simulation
    A define specifies how often a monitor operation should return an error.
    The idea behind this binary is to help simulate scale by running thousands
    of these processes at once and determining cpu utilization.
    
    Signed-off-by: Steven Dake <sdake@redhat.com>

---
+++ tests/sim_recovery.c
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2012 Red Hat, Inc.
+ *
+ * Authors: Steven Dake <sdake@redhat.com>
+ *
+ * This file is part of pacemaker-cloud.
+ *
+ * pacemaker-cloud is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * pacemaker-cloud is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with pacemaker-cloud.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <check.h>
+#include <stdlib.h>
+
+#include <qb/qbdefs.h>
+#include <qb/qblog.h>
+#include <qb/qbloop.h>
+
+#include "cape.h"
+#include "trans.h"
+
+#define FAILURE_PERCENT 90
+
+void transport_resource_action(struct assembly *a,
+        struct resource *resource,
+        struct pe_operation *op)
+{
+	qb_enter();
+
+	if (strcmp(op->method, "monitor") == 0 && op->interval > 0) {
+	if ((random() % 100) < FAILURE_PERCENT) {
+		resource_action_completed(op, OCF_NOT_RUNNING);
+	} else {
+		resource_action_completed(op, op->target_outcome);
+	}
+	} else {
+	resource_action_completed(op, op->target_outcome);
+	}
+
+	qb_leave();
+
+	return;
+}
+
+void *transport_connect(struct assembly *a)
+{
+	qb_enter();
+
+	qb_leave();
+
+	return NULL;
+}
+
+void transport_disconnect(struct assembly *a)
+{
+	qb_enter();
+
+	qb_leave();
+}
+
+void transport_del(void *transport);
+
+int32_t instance_create(struct assembly *assembly)
+{
+	qb_enter();
+
+	recover_state_set(&assembly->recover, RECOVER_STATE_UNKNOWN);
+
+	recover_state_set(&assembly->recover, RECOVER_STATE_RUNNING);
+
+	qb_leave();
+	return 0;
+}
+
+int instance_stop(struct assembly *a)
+{
+	qb_enter();
+	qb_leave();
+
+	return 0;
+}
+
